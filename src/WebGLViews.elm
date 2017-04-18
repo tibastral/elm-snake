51af7bb83ff5e296225de2f7e8815b37751e8f95
Andrey Kuzmin <unsoundscapes@gmail.com>
2017-04-18 20:15:59 +0200

    Added lighting

--- src/WebGLViews.elm
+++ src/WebGLViews.elm
@@ -4,12 +4,14 @@ import Types exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import WebGL exposing (Mesh, Shader)
-import Math.Vector3 exposing (Vec3, vec3)
+import Math.Vector3 as Vec3 exposing (Vec3, vec3)
 import Math.Matrix4 exposing (Mat4, mul)
 
 
 type alias Attributes =
-    { position : Vec3 }
+    { position : Vec3
+    , normal : Vec3
+    }
 
 
 camera : Float -> Mat4
@@ -25,30 +27,45 @@ camera ratio =
             vec3 c c 0
     in
         mul (Math.Matrix4.makePerspective 45 ratio 0.01 100)
-            (Math.Matrix4.makeLookAt eye center Math.Vector3.j)
+            (Math.Matrix4.makeLookAt eye center Vec3.j)
+
+
+attributes : Vec3 -> Vec3 -> Vec3 -> ( Attributes, Attributes, Attributes )
+attributes p1 p2 p3 =
+    let
+        v1 =
+            Vec3.sub p1 p2
+
+        v2 =
+            Vec3.sub p1 p3
+
+        normal =
+            Vec3.cross v1 v2 |> Vec3.normalize
+    in
+        ( Attributes p1 normal, Attributes p2 normal, Attributes p3 normal )
 
 
 cube : Mesh Attributes
 cube =
     WebGL.triangles
-        [ -- top
-          ( Attributes (vec3 -0.5 0.5 -0.5), Attributes (vec3 -0.5 0.5 0.5), Attributes (vec3 0.5 0.5 0.5) )
-        , ( Attributes (vec3 0.5 0.5 0.5), Attributes (vec3 0.5 0.5 -0.5), Attributes (vec3 -0.5 0.5 -0.5) )
-          -- back
-        , ( Attributes (vec3 -0.5 0.5 0.5), Attributes (vec3 0.5 0.5 0.5), Attributes (vec3 -0.5 -0.5 0.5) )
-        , ( Attributes (vec3 -0.5 -0.5 0.5), Attributes (vec3 0.5 -0.5 0.5), Attributes (vec3 0.5 0.5 0.5) )
+        [ -- back
+          attributes (vec3 -0.5 0.5 -0.5) (vec3 -0.5 0.5 0.5) (vec3 0.5 0.5 0.5)
+        , attributes (vec3 0.5 0.5 0.5) (vec3 0.5 0.5 -0.5) (vec3 -0.5 0.5 -0.5)
+          -- top
+        , attributes (vec3 -0.5 0.5 0.5) (vec3 -0.5 -0.5 0.5) (vec3 0.5 0.5 0.5)
+        , attributes (vec3 -0.5 -0.5 0.5) (vec3 0.5 -0.5 0.5) (vec3 0.5 0.5 0.5)
           -- right
-        , ( Attributes (vec3 0.5 0.5 0.5), Attributes (vec3 0.5 -0.5 0.5), Attributes (vec3 0.5 -0.5 -0.5) )
-        , ( Attributes (vec3 0.5 -0.5 -0.5), Attributes (vec3 0.5 0.5 -0.5), Attributes (vec3 0.5 0.5 0.5) )
+        , attributes (vec3 0.5 0.5 0.5) (vec3 0.5 -0.5 -0.5) (vec3 0.5 -0.5 0.5)
+        , attributes (vec3 0.5 -0.5 -0.5) (vec3 0.5 0.5 0.5) (vec3 0.5 0.5 -0.5)
           -- left
-        , ( Attributes (vec3 -0.5 0.5 -0.5), Attributes (vec3 -0.5 0.5 0.5), Attributes (vec3 -0.5 -0.5 0.5) )
-        , ( Attributes (vec3 -0.5 0.5 -0.5), Attributes (vec3 -0.5 -0.5 0.5), Attributes (vec3 -0.5 -0.5 -0.5) )
-          -- front
-        , ( Attributes (vec3 -0.5 0.5 -0.5), Attributes (vec3 0.5 0.5 -0.5), Attributes (vec3 0.5 -0.5 -0.5) )
-        , ( Attributes (vec3 -0.5 0.5 -0.5), Attributes (vec3 0.5 -0.5 -0.5), Attributes (vec3 -0.5 -0.5 -0.5) )
+        , attributes (vec3 -0.5 0.5 -0.5) (vec3 -0.5 0.5 0.5) (vec3 -0.5 -0.5 0.5)
+        , attributes (vec3 -0.5 0.5 -0.5) (vec3 -0.5 -0.5 0.5) (vec3 -0.5 -0.5 -0.5)
           -- bottom
-        , ( Attributes (vec3 -0.5 -0.5 -0.5), Attributes (vec3 -0.5 -0.5 0.5), Attributes (vec3 0.5 -0.5 0.5) )
-        , ( Attributes (vec3 0.5 -0.5 0.5), Attributes (vec3 0.5 -0.5 -0.5), Attributes (vec3 -0.5 -0.5 -0.5) )
+        , attributes (vec3 -0.5 0.5 -0.5) (vec3 0.5 0.5 -0.5) (vec3 0.5 -0.5 -0.5)
+        , attributes (vec3 -0.5 0.5 -0.5) (vec3 0.5 -0.5 -0.5) (vec3 -0.5 -0.5 -0.5)
+          -- front
+        , attributes (vec3 -0.5 -0.5 -0.5) (vec3 0.5 -0.5 0.5) (vec3 -0.5 -0.5 0.5)
+        , attributes (vec3 0.5 -0.5 0.5) (vec3 -0.5 -0.5 -0.5) (vec3 0.5 -0.5 -0.5)
         ]
 
 
@@ -59,8 +76,8 @@ field =
             toFloat config.max + 1
     in
         WebGL.triangles
-            [ ( Attributes (vec3 0 s -0.5), Attributes (vec3 s s -0.5), Attributes (vec3 0 0 -0.5) )
-            , ( Attributes (vec3 0 0 -0.5), Attributes (vec3 s 0 -0.5), Attributes (vec3 s s -0.5) )
+            [ attributes (vec3 0 s -0.5) (vec3 0 0 -0.5) (vec3 s s -0.5)
+            , attributes (vec3 0 0 -0.5) (vec3 s 0 -0.5) (vec3 s s -0.5)
             ]
 
 
@@ -101,25 +118,38 @@ snakeView ratio snake =
         |> List.map (vertebraView ratio)
 
 
-vertexShader : Shader Attributes Uniforms {}
+type alias Varying =
+    { vlighting : Float
+    }
+
+
+vertexShader : Shader Attributes Uniforms Varying
 vertexShader =
     [glsl|
         attribute vec3 position;
+        attribute vec3 normal;
         uniform vec3 offset;
         uniform mat4 camera;
+        varying highp float vlighting;
         void main () {
+            highp float ambientLight = 0.4;
+            highp float directionalLight = 0.6;
+            highp vec3 directionalVector = normalize(vec3(0, -2, 3));
             gl_Position = camera * vec4(position + offset, 1.0);
+            highp float directional = max(dot(normal, directionalVector), 0.0);
+            vlighting = ambientLight + directional * directionalLight;
         }
     |]
 
 
-fragmentShader : Shader {} Uniforms {}
+fragmentShader : Shader {} Uniforms Varying
 fragmentShader =
     [glsl|
         precision mediump float;
+        varying highp float vlighting;
         uniform vec3 color;
         void main () {
-            gl_FragColor = vec4(color, 1.0);
+            gl_FragColor = vec4(color * vlighting, 1.0);
         }
     |]
 
