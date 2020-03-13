6a6bd0bd221683065a60a7f015e3a06c90000a4f
Thibaut Assus <thibaut@milesrock.com>
2020-03-13 10:48:04 +0100

    Without old files

--- src/Views.elm
+++
@@ -1,8 +0,0 @@
-module Views exposing (appleView, scoreView, snakeView, spriteView, toPx, vertebraView, view, wallsView, worldView)
-
-import Html exposing (..)
-import Html.Attributes exposing (..)
-
-
-
--- import Types exposing (..)
